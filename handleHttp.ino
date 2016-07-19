44b9aec33b9229c9c3e531f6e52d0b0a50cc8217
lukepereira <lukejoepereira@gmail.com>
2016-07-19 00:13:34 -0400

    Add files via upload

---
+++ handleHttp.ino
@@ -0,0 +1,165 @@
+/** Handle root or redirect to captive portal */
+void handleRoot() {
+  if (captivePortal()) { // If caprive portal redirect instead of displaying the page.
+    return;
+  }
+  server.sendHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+  server.sendHeader("Pragma", "no-cache");
+  server.sendHeader("Expires", "-1");
+  server.setContentLength(CONTENT_LENGTH_UNKNOWN);
+  server.send(200, "text/html", ""); // Empty content inhibits Content-length header so we have to close the socket ourselves.
+
+  server.sendContent(
+  "<html><head><title>Hacker Union</title></head>"
+  "<body><center><br><h1>Welcome</h1><br>"
+  "<table id='hnmain' border='0' cellpadding='0' cellspacing='0' width='85%' bgcolor='#f6f6ef'>"
+  "<tr><td bgcolor='#ff6600'>"
+  "<table border='0' cellpadding='0' cellspacing='0' width='100%' style='padding:2px'>"
+  "<tr><td style='width:18px;padding-right:4px'>"
+  "<a href='/'><img width='18' height='18' style='border:1px #ffffff solid;background-color:#D7F1FA;'></a>"
+  "</td><td style='line-height:15pt; height:10px;'><span class='pagetop'><b>&nbsp; Hacker Union &nbsp;&nbsp;</b>"
+  "<a href='/board'>posts</a> | <a href='/wifi'>wifi</a> | <a href='/'>about</a> "
+  "</span></td></tr></table></td></tr><tr><td><table border='1'></td></tr>"
+  "<tr style='height:20px'></tr>"
+  "<tr><td>Res nolunt diu male administrari &mdash; Things refuse to be mismanaged long</td></tr>"
+  "<tr style='height:20px'></tr><tr><td>Stewardship is an ethic that embodies the responsible " 
+  "planning and management of resources. The concepts of stewardship can be applied to the environment, "
+  "economics, health, property, and information.</td></tr><tr style='height:20px'></tr>"
+  "<tr><td>Industrial democracy refers to the organization model "
+  "in which workplaces are run directly by the people who work in them in place of private or state ownership "
+  "of the means of production. Deliberative democracy is a form of democracy where deliberation is central "
+  "to decision-making which differs from traditional democratic theory in that authentic "
+  "deliberation, not mere voting, is the primary source of legitimacy for a law."
+  "</td></tr><tr style='height:20px'></tr>"
+  "<tr><td>There are no political solutions, only technological ones; the rest is propaganda. &mdash; Jacques Ellul"
+  "</td></tr><tr style='height:20px'><tr><td><b>About The Prototype</b></td></tr><tr><td>"
+  "Hacker Union is an open wifi network used for group decision-making and communication within a local area. "
+  "This web server is being hosted on a small and portable wifi thumb drive powered by battery or USB. "
+  "Posts are submitted and voted on. The highest ranking proposition is then group edited and made into a coherent "
+  "and concise legislation. "
+  "The module shares and collates content with other nearby HU wifi chips, forming a decentralized mesh network. "
+  "Connection to the internet is possible given an SSID, password, and port-forwarding access on a router. "
+  "With an internet connection, the module will upload and backup all content to the internet.</td></tr>"
+  "<tr style='height:20px'></tr></table></form></td></tr></table><br><br>"
+  );
+  if (server.client().localIP() == apIP) {
+    server.sendContent(String("<p><small>You are connected through the soft AP: ") + softAP_ssid + "</small></p>");
+  } else {
+    server.sendContent(String("<p><small>You are connected through the wifi network: ") + ssid + "</small></p>");
+  }
+  server.sendContent(
+  "</body></html>"
+  );
+  server.client().stop(); // Stop is needed because we sent no content length
+}
+
+/** Redirect to captive portal if we got a request for another domain. Return true in that case so the page handler do not try to handle the request again. */
+boolean captivePortal() {
+  if (!isIp(server.hostHeader()) && server.hostHeader() != (String(myHostname)+".local")) {
+    Serial.println("Request redirected to captive portal");
+    server.sendHeader("Location", String("http://") + toStringIp(server.client().localIP()), true);
+    server.send ( 302, "text/plain", ""); // Empty content inhibits Content-length header so we have to close the socket ourselves.
+    server.client().stop(); // Stop is needed because we sent no content length
+    return true;
+  }
+  return false;
+}
+
+/** Wifi config page handler */
+void handleWifi() {
+  server.sendHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+  server.sendHeader("Pragma", "no-cache");
+  server.sendHeader("Expires", "-1");
+  server.setContentLength(CONTENT_LENGTH_UNKNOWN);
+  server.send(200, "text/html", ""); // Empty content inhibits Content-length header so we have to close the socket ourselves.
+  server.sendContent(
+    "<html><head></head><body>"
+    "<h1>Wifi config</h1>"
+  );
+  if (server.client().localIP() == apIP) {
+    server.sendContent(String("<p>You are connected through the soft AP: ") + softAP_ssid + "</p>");
+  } else {
+    server.sendContent(String("<p>You are connected through the wifi network: ") + ssid + "</p>");
+  }
+  server.sendContent(
+    "\r\n<br />"
+    "<table><tr><th align='left'>SoftAP config</th></tr>"
+  );
+  server.sendContent(String() + "<tr><td>SSID " + String(softAP_ssid) + "</td></tr>");
+  server.sendContent(String() + "<tr><td>IP " + toStringIp(WiFi.softAPIP()) + "</td></tr>");
+  server.sendContent(
+    "</table>"
+    "\r\n<br />"
+    "<table><tr><th align='left'>WLAN config</th></tr>"
+  );
+  server.sendContent(String() + "<tr><td>SSID " + String(ssid) + "</td></tr>");
+  server.sendContent(String() + "<tr><td>IP " + toStringIp(WiFi.localIP()) + "</td></tr>");
+    server.sendContent(
+    "</table>"
+    "\r\n<br /><form method='POST' action='wifisave'><h4>Connect to network:</h4>"
+    "<p> (For internet, configure port forwarding on router using WLAN IP) </p>"
+    "<input type='text' placeholder='network' name='n'/>"
+    "<br /><input type='password' placeholder='password' name='p'/>"
+    "<br /><input type='submit' value='Connect/Disconnect'/></form>"
+    "\r\n<br />"
+    "<table><tr><th align='left'>WLAN list (refresh if any missing)</th></tr>"
+  );
+
+  Serial.println("scan start");
+  int n = WiFi.scanNetworks();
+  Serial.println("scan done");
+  if (n > 0) {
+    for (int i = 0; i < n; i++) {
+      server.sendContent(String() + "\r\n<tr><td>SSID " + WiFi.SSID(i) + String((WiFi.encryptionType(i) == ENC_TYPE_NONE)?" ":" *") + " (" + WiFi.RSSI(i) + ")</td></tr>");
+    }
+  } else {
+    server.sendContent(String() + "<tr><td>No WLAN found</td></tr>");
+  }
+  server.sendContent(
+    "</table>"
+    "\r\n<br />"
+    "<p>Return to the <a href='/'>home page</a>.</p>"
+    "</body></html>"
+  );
+  server.client().stop(); // Stop is needed because we sent no content length
+}
+
+
+
+/** Handle the WLAN save form and redirect to WLAN config page again */
+void handleWifiSave() {
+  Serial.println("wifi save");
+  server.arg("n").toCharArray(ssid, sizeof(ssid) - 1);
+  server.arg("p").toCharArray(password, sizeof(password) - 1);
+  server.sendHeader("Location", "wifi", true);
+  server.sendHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+  server.sendHeader("Pragma", "no-cache");
+  server.sendHeader("Expires", "-1");
+  server.send ( 302, "text/plain", "");  // Empty content inhibits Content-length header so we have to close the socket ourselves.
+  server.client().stop(); // Stop is needed because we sent no content length
+  saveCredentials();
+  connect = strlen(ssid) > 0; // Request WLAN connect with new credentials if there is a SSID
+}
+
+void handleNotFound() {
+  if (captivePortal()) { // If caprive portal redirect instead of displaying the error page.
+    return;
+  }
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += ( server.method() == HTTP_GET ) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+
+  for ( uint8_t i = 0; i < server.args(); i++ ) {
+    message += " " + server.argName ( i ) + ": " + server.arg ( i ) + "\n";
+  }
+  server.sendHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+  server.sendHeader("Pragma", "no-cache");
+  server.sendHeader("Expires", "-1");
+  server.send ( 404, "text/plain", message );
+}
+
---
+++ tools.ino
@@ -0,0 +1,94 @@
+/** Is this an IP? */
+boolean isIp(String str) {
+  for (int i = 0; i < str.length(); i++) {
+    int c = str.charAt(i);
+    if (c != '.' && (c < '0' || c > '9')) {
+      return false;
+    }
+  }
+  return true;
+}
+
+/** IP to String? */
+String toStringIp(IPAddress ip) {
+  String res = "";
+  for (int i = 0; i < 3; i++) {
+    res += String((ip >> (8 * i)) & 0xFF) + ".";
+  }
+  res += String(((ip >> 8 * 3)) & 0xFF);
+  return res;
+}
+
+boolean alreadyVoted(String metaData, String ip){
+  Serial.println("alreadVoted function");
+  Serial.println(metaData);
+  File fp = SPIFFS.open(metaData, "a+");
+  char ipBuffer[17];
+  char garbage[5];
+  int voteCount = 0;
+  while (fp.available() > 5) { 
+    int ipIndex = fp.readBytesUntil('\0', ipBuffer, 16);
+    ipBuffer[ipIndex] = '\0'; 
+    if (ip == String(ipBuffer)) {
+      fp.close();
+      return true;
+    }
+    ++voteCount;
+  }
+  //Store voteCount at end of file
+  fp.write(voteCount);
+  fp.write('\0');
+  fp.close();
+  return false;
+}
+
+/*
+// You must free the result if result is non-NULL.
+char *str_replace(char *orig, char *rep, char *with) {
+    char *result; // the return string
+    char *ins;    // the next insert point
+    char *tmp;    // varies
+    int len_rep;  // length of rep
+    int len_with; // length of with
+    int len_front; // distance between rep and end of last rep
+    int count;    // number of replacements
+
+    if (!orig)
+        return NULL;
+    if (!rep)
+        rep = "";
+    len_rep = strlen(rep);
+    if (!with)
+        with = "";
+    len_with = strlen(with);
+
+    ins = orig;
+    for (count = 0; tmp = strstr(ins, rep); ++count) {
+        ins = tmp + len_rep;
+    }
+
+    // first time through the loop, all the variable are set correctly
+    // from here on,
+    //    tmp points to the end of the result string
+    //    ins points to the next occurrence of rep in orig
+    //    orig points to the remainder of orig after "end of rep"
+    tmp = result = malloc(strlen(orig) + (len_with - len_rep) * count + 1);
+
+    if (!result)
+        return NULL;
+
+    while (count--) {
+        ins = strstr(orig, rep);
+        len_front = ins - orig;
+        tmp = strncpy(tmp, orig, len_front) + len_front;
+        tmp = strcpy(tmp, with) + len_with;
+        orig += len_front + len_rep; // move to next "end of rep"
+    }
+    strcpy(tmp, orig);
+    return result;
+}
+
+void escapeHTML(char * raw){
+   &raw = str_replace(raw, "<", "&lt;");
+}
+*/
